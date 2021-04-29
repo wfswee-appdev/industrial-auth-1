53eeb7a786d52346dc1bd3b72a4c235720e026aa
wfswee <wfswee@chicagobooth.edu>
2021-04-29 05:55:46 +0000

    before_action to stop follow_requests#show if pending and not recipient or sender

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,6 +1,7 @@
 class FollowRequestsController < ApplicationController
   before_action :set_follow_request, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [ :edit, :destroy ]
+  before_action :user_can_only_see_their_own_pending_follow_requests, only: :show
 
   # GET /follow_requests or /follow_requests.json
   def index
@@ -70,8 +71,13 @@ class FollowRequestsController < ApplicationController
     end
 
     def ensure_current_user_is_owner
-     p @follow_request
       if current_user.id != @follow_request.sender_id
+        redirect_back fallback_location: root_url, notice: "You are not authorized for this."
+      end
+    end
+
+    def user_can_only_see_their_own_pending_follow_requests
+      if @follow_request.status == "pending" && (current_user.id != @follow_request.recipient_id && current_user.id != @follow_request.sender_id)
         redirect_back fallback_location: root_url, notice: "You are not authorized for this."
       end
     end
